afa744a2459a9cc207646900c728b0524dcd5abd
admin-supreme <ayushroy18102010@gmail.com>
2026-03-24 18:46:42 +0530

    Update d3vtools.js

--- d3vtools.js
+++ d3vtools.js
@@ -698,7 +698,7 @@ function treeRows(entries) {
   });
 
     function escRegex(s) {
-  return String(s).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  return String(s).replace(/[.*+?^\${}()|[\]\\]/g, '\\$&');
 }
 
     function snippet(text, term) {
